28c2b6c868237218bb2a461928a96f630a5548b2
Yassef AyubBoggeano <ayub.yassef@icloud.com>
2023-05-25 14:30:42 -0700

    core submission

--- flask/playground/server.py
+++ flask/playground/server.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/play')
 def challenge_one():
-    return render_template('index.html', message="Simply the Best", reps=2)
+    return render_template('index.html', num=3, color="red")
+
+@app.route('/play/<int:num>')
+def challenge_two(num):
+    return render_template('index.html', num=num, color="green")
+
+@app.route('/play/<int:num>/<string:color>')
+def challenge_three(num, color):
+    return render_template('index.html', num=num, color=color)
 
 if __name__=="__main__":
     app.run(debug=True)
