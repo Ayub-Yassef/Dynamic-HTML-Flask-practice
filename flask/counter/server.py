cca2d48372131cf08ff2ef635ecfe31bd85edfe2
Yassef AyubBoggeano <ayub.yassef@icloud.com>
2023-05-31 16:15:30 -0700

    before session end

---
+++ flask/counter/server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, session, redirect
+app = Flask(__name__)
+app.secret_key = "deny, deny, deny"
+@app.route('/')
+def index():
+    if "count" not in session:
+        session["count"] = 0
+    else:
+        session["count"] += 1
+    return render_template("index.html")
+@app.route('/restart')
+def restart():
+    session.clear()
+    return redirect('/')
+if __name__=="__main__":
+    app.run(debug=True)
