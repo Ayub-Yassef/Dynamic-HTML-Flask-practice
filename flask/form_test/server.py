4991f6e7b1b5f7269019f8b970fd10b3e76bbba2
Yassef AyubBoggeano <ayub.yassef@icloud.com>
2023-06-03 12:35:03 -0700

    show template added

--- flask/form_test/server.py
+++ flask/form_test/server.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
-# our index route will handle rendering our form
+app.secret_key = "illuminati_password_123"
 @app.route('/')
 def index():
     return render_template("index.html")
 @app.route('/users', methods=['POST'])
 def create_user():
     print("Got Post Info")
-    print(request.form)
-    # Never render a template on a POST request.
-    # Instead we will redirect to our index route.
-    return redirect('/')
+    session['username'] = request.form['name']
+    session['useremail'] = request.form['email']
+    return redirect('/show')
+@app.route('/show')
+def show_user():
+    return render_template('show.html', name_on_template=session['username'], email_on_template=session['useremail'])
 if __name__ == "__main__":
     app.run(debug=True)
 
