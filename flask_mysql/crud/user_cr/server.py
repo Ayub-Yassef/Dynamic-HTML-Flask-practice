a7300bb201a1e1ecd1d649b9172e68c16ca9586d
Yassef AyubBoggeano <ayub.yassef@icloud.com>
2023-06-08 12:48:52 -0700

    create and read application

--- flask_mysql/crud/user_cr/server.py
+++ flask_mysql/crud/user_cr/server.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template, request, redirect
-from users import User
-app - Flask(__name__)
+from user import User
+app = Flask(__name__)
 @app.route('/')
-def index():
-    return redirect('/users')
-@app.route('/users')
-def users():
-    return render_template("users.html", users=User.fetch_info())
+def create_profile():
+    users=User.get_all_users()
+    print(users)
+    return render_template("read.html", users=users)
+
+@app.route('/form_submission', methods=["POST"])
+def profile_generated():
+    data={
+        "first_name": request.form["first_name"],
+        "last_name": request.form["last_name"],
+        "email": request.form["email"]
+    }
+    User.add_user(data)
+    return redirect("/")
+
+@app.route('/create')
+def members_display():
+    return render_template("create.html")
 
 if __name__=="__main__":
     app.run(debug=True)
