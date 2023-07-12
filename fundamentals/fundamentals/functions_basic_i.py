aa78709d326f123fd9b97f0969746a9ee912b9fd
Yassef AyubBoggeano <ayub.yassef@icloud.com>
2023-07-12 12:38:29 -0600

    fundamentals in py to revise for myself

---
+++ fundamentals/fundamentals/functions_basic_i.py
@@ -0,0 +1,28 @@
+#1 Prediction: 5
+def number_of_food_groups():
+    return 5
+print(number_of_food_groups())
+
+#2 Prediction: 'number_of_days_in_a_week_silicon_or_triangle_sides' Undefined
+def number_of_military_branches():
+    return 5
+print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+
+#3 Prediction: 5 (remember 'return' automatically exits you from the function call)
+def number_of_books_on_hold():
+    return 5
+    return 10
+print(number_of_books_on_hold())
+
+#4 Prediction: 5
+def number_of_fingers():
+    return 5
+    print(10)
+print(number_of_fingers())
+
+#5 Prediction: 5 then None due to no 'return' statement in the function
+def number_of_great_lakes():
+    print(5)
+x = number_of_great_lakes()
+print(x)
+
