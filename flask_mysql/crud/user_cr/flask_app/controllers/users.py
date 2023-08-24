1b782a897ef01c7d86e23a3a5990be24fb99cc26
Yassef AyubBoggeano <ayub.yassef@icloud.com>
2023-08-24 08:22:54 -0700

    revision and condensation of code

---
+++ flask_mysql/crud/user_cr/flask_app/controllers/users.py
@@ -0,0 +1,54 @@
+from flask_app import app
+from flask import render_template, redirect, request, session
+from flask_app.models import user # import entire file, rather than class, to avoid circular imports
+
+# Create Users Controller
+class Users:
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+    @classmethod
+    def fetch_info(cls):
+
+# Read Users Controller
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Update Users Controller
+
+
+
+# Delete Users Controller
+
+
+# Notes:
+# 1 - Use meaningful names
+# 2 - Do not overwrite function names
+# 3 - No matchy, no worky
+# 4 - Use consistent naming conventions 
+# 5 - Keep it clean
+# 6 - Test every little line before progressing
+# 7 - READ ERROR MESSAGES!!!!!!
+# 8 - Error messages are found in the browser and terminal
+
+
+
+
+# How to use path variables:
+# @app.route('/<int:id>')
+# def index(id):
+#     user_info = user.User.get_user_by_id(id)
+#     return render_template('index.html', user_info)
+
+# Converter -	Description
+# string -	Accepts any text without a slash (the default).
+# int -	Accepts integers.
+# float -	Like int but for floating point values.
+# path 	-Like string but accepts slashes.
--- flask_mysql/crud/user_cr/server.py
+++ flask_mysql/crud/user_cr/server.py
@@ -1,19 +1,10 @@
-from flask import Flask, render_template, request, redirect
-from user import User
-app = Flask(__name__)
+from flask_app import app
+from flask_app.controllers import render_template, request, redirect
 @app.route('/')
-def create_profile():
-    these_users=User.get_all_users()
-    return render_template("read.html", users=these_users)#Users is a named argument. these_users is its value
-
-@app.route('/application_process', methods=["POST"])
-def application_process():
-    User.add_user(request.form)
-    return redirect("/")
-
-@app.route('/application_page')
-def application_page():
-    return render_template("create.html")
-
+def index():
+    return redirect('/users')
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
+
+# debug needs to be set to False when deployed.
+# We shared a video showing how the information leaked by this feature and help hackers.
