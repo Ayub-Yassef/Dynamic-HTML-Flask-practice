678f66d1e6840785d8442186968ef977a21512b4
Yassef AyubBoggeano <ayub.yassef@icloud.com>
2023-08-27 14:18:35 -0700

    renamed file

--- flask/html_table/server.py
+++ flask/html_table/server.py
@@ -7,7 +7,7 @@ def index():
         {'first_name' : 'Michael', 'last_name' : 'Choi'},
         {'first_name' : 'John', 'last_name' : 'Supsupin'},
         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+        {'first_name' : 'Yassef', 'last_name' : 'Ayub Boggeano'}
     ]
     return render_template("index.html",users=users)
 
